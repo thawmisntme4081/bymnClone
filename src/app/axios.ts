fd953fb903412cfce5e081db114fda07e93b92fa
ntuanthanh <ntuanthanh184@gmail.com>
2023-08-11 12:42:12 +0700

    Edit env

--- src/app/axios.ts
+++ src/app/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const publicAxios = axios.create({
-  baseURL: process.env.SERVER_URL,
+  baseURL: process.env.REACT_APP_SERVER_URL,
   headers: {
     'Content-type': 'application/json',
   },
